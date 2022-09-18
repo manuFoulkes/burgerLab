b691f1a59b39daddd047d2011bca6ab50904ed99
Manuel Foulkes <manuel.foulkes90@gmail.com>
2022-09-18 18:11:52 -0300

    Interfaz Burger - Template del componente Burger-List

---
+++ src/app/burger-list/Burger.ts
@@ -0,0 +1,11 @@
+
+
+
+export interface Burger {
+    name: string;
+    type: string;
+    price: number;
+    stock: number;
+    img: string;
+    clearance: boolean;
+}
--- src/app/burger-list/burger-list.component.ts
+++ src/app/burger-list/burger-list.component.ts
@@ -1,19 +1,49 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Burger } from './Burger';
 @Component({
   selector: 'app-burger-list',
   templateUrl: './burger-list.component.html',
   styleUrls: ['./burger-list.component.css']
 })
-export class BurgerListComponent implements OnInit {
 
-  burger = {
-    'type': 'Carne',
-    'name': 'Con Cheddar',
-    'price': 800,
-    'stock': 100,
-    'img': 'assets/images/cheddar_burger.jpg'
-  }
+export class BurgerListComponent implements OnInit {
+ 
+  burgers: Burger[] = [
+    {
+      type: 'Carne',
+      name: 'Con Cheddar',
+      price: 800,
+      stock: 15,
+      img: 'assets/images/cheddar_burger.jpg',
+      clearance: false,
+    },
+    {
+      type: 'Carne',
+      name: 'Chori Burga',
+      price: 750,
+      stock: 15,
+      img: 'assets/images/cheddar_burger.jpg',
+      clearance: true,
+    },
+    {
+      type: 'Veggie',
+      name: 'Lentejas',
+      price: 600,
+      stock: 10,
+      img: 'assets/images/cheddar_burger.jpg',
+      clearance: true,
+    },
+    {
+      type: 'Veggie',
+      name: 'Lentejas',
+      price: 600,
+      stock: 0,
+      img: 'assets/images/cheddar_burger.jpg',
+      clearance: false,
+    },
+  ];
+  
+  
 
   constructor() { }
 
